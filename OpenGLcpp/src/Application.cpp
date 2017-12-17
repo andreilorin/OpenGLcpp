49c4e130ed207f3a7a83bceab0130239b70719f7
andrei lorin <andreilorin@gmail.com>
2017-12-17 20:24:10 +0000

    created index buffer. using index buffer to draw a square

--- OpenGLcpp/src/Application.cpp
+++ OpenGLcpp/src/Application.cpp
@@ -71,11 +71,13 @@ int main(void)
 	float positions[] = {
 		-0.5f, -0.5f,
 		 0.5f, -0.5f,
-		 0.5f,  0.5f,
+		 0.5f,  0.5f,		
+		-0.5f,  0.5f
+	};
 
-		 0.5f,  0.5f,
-		-0.5f,  0.5f,
-		-0.5f, -0.5f
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
@@ -85,6 +87,11 @@ int main(void)
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -118,7 +125,8 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);	
